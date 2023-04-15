b977a85801a2f006b11ac69f7ecd00d3cec39ad3
TTranchese <tommaso.tranchese.96@gmail.com>
2023-04-15 12:12:06 +0200

    secondo commit

--- src/main/java/dev/ttranchese/Payroll/Employee.java
+++ src/main/java/dev/ttranchese/Payroll/Employee.java
@@ -1,23 +1,26 @@
 package dev.ttranchese.Payroll;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 import java.util.Objects;
 
 @Entity
-public class Employee {
-	private @Id @GeneratedValue Long id;
+class Employee {
+
+	private @Id	@GeneratedValue Long id;
 	private String name;
 	private String role;
 
-	Employee(){}
-	Employee(String name, String role){
-		this.name=name;
-		this.role=role;
+	Employee() {}
+
+	Employee(String name, String role) {
+
+		this.name = name;
+		this.role = role;
 	}
+
 	public Long getId() {
 		return this.id;
 	}
---
+++ src/main/java/dev/ttranchese/Payroll/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.ttranchese.Payroll;
+
+ class EmployeeNotFoundException extends RuntimeException {
+	public EmployeeNotFoundException(Long id) {
+		super("Could not find employee "+id);
+	}
+}
